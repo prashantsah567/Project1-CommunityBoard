374aac820a4e88477e1387f3e15a60de08890845
Prashant Sah <prashantsah567@gmail.com>
2023-02-23 18:18:25 -0500

    card view are alligned to center and banner added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import CardView from './components/CardView'
 import dataScience from './assets/dataScience.jpg'
+import banner from './assets/banner.jpg'
 
 function App() {
 
   return (
     <>
+    <img src={banner} alt="img cannot be loaded" class="banner"/>
     <h1>Software engineering Community Board</h1>
       <div className="App">
         <CardView name="Data Science" field="Software engineering" image={dataScience} btnLink="https://www.oracle.com/what-is-data-science/" />

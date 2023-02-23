dc95bac249a2ffcdbd3419ece19a24e3ae1138a6
Prashant Sah <prashantsah567@gmail.com>
2023-02-22 20:55:41 -0500

    sample card layout created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import './App.css'
+import CardView from './components/CardView'
+import dataScience from './assets/dataScience.jpg'
 
 function App() {
 
   return (
-    <div className="App">
-      <h1>All set</h1>
-    </div>
+    <>
+    <h1>Software engineering Community Board</h1>
+      <div className="App">
+        <CardView name="Data Science" field="Software engineering" image={dataScience} btnLink="https://www.oracle.com/what-is-data-science/" />
+        <CardView name="Data Science" field="Software engineering" image={dataScience} btnLink="https://www.oracle.com/what-is-data-science/" />
+        <CardView name="Data Science" field="Software engineering" image={dataScience} btnLink="https://www.oracle.com/what-is-data-science/" />
+        <CardView name="Data Science" field="Software engineering" image={dataScience} btnLink="https://www.oracle.com/what-is-data-science/" />
+        <CardView name="Data Science" field="Software engineering" image={dataScience} btnLink="https://www.oracle.com/what-is-data-science/" />
+        <CardView name="Data Science" field="Software engineering" image={dataScience} btnLink="https://www.oracle.com/what-is-data-science/" />
+        <CardView name="Data Science" field="Software engineering" image={dataScience} btnLink="https://www.oracle.com/what-is-data-science/" />
+        <CardView name="Data Science" field="Software engineering" image={dataScience} btnLink="https://www.oracle.com/what-is-data-science/" />
+        <CardView name="Data Science" field="Software engineering" image={dataScience} btnLink="https://www.oracle.com/what-is-data-science/" />
+        <CardView name="Data Science" field="Software engineering" image={dataScience} btnLink="https://www.oracle.com/what-is-data-science/" />
+      </div>
+    </>
   )
 }
 
---
+++ src/components/CardView.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './CardView.css';
+
+const CardView = (props) =>{
+    return(
+        <div className='container'> 
+            <div className="CardView">
+                <img src={props.image} alt="couldn't load" class="image"/>
+                <h3>{props.name}</h3>
+                <h5>{props.field}</h5>
+                <a href={props.btnLink} target="_blank"><button>Learn More</button></a>
+            </div>
+        </div>
+    )
+}
+
+export default CardView;

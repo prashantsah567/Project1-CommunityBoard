82eee487358d3a21ef3e04da67de76966946f3c4
Prashant Sah <prashantsah567@gmail.com>
2023-02-23 19:18:00 -0500

    all button links added

--- src/App.jsx
+++ src/App.jsx
@@ -19,16 +19,16 @@ function App() {
     <img src={banner} alt="img cannot be loaded" class="banner"/>
     <h1>Software engineering Community Board</h1>
       <div className="App">
-        <CardView name="3D Designer" field="UI/UX design" image={designer} btnLink="https://www.oracle.com/what-is-data-science/" />
-        <CardView name="Frontend Developer" field="Software development" image={frontend} btnLink="https://www.oracle.com/what-is-data-science/" />
-        <CardView name="Mobile Developer" field="Application development" image={mobileDeveloper} btnLink="https://www.oracle.com/what-is-data-science/" />
-        <CardView name="Database developer" field="Database engineering" image={database} btnLink="https://www.oracle.com/what-is-data-science/" />
-        <CardView name="Backend Developer" field="Software development" image={backend} btnLink="https://www.oracle.com/what-is-data-science/" />
+        <CardView name="3D Designer" field="UI/UX design" image={designer} btnLink="https://www.sketchup.com/" />
+        <CardView name="Frontend Developer" field="Software development" image={frontend} btnLink="https://developer.mozilla.org/en-US/docs/Learn/Front-end_web_developer" />
+        <CardView name="Mobile Developer" field="Application development" image={mobileDeveloper} btnLink="https://www.codecademy.com/catalog/subject/mobile-development" />
+        <CardView name="Database developer" field="Database engineering" image={database} btnLink="https://about.gitlab.com/handbook/engineering/development/database/" />
+        <CardView name="Backend Developer" field="Software development" image={backend} btnLink="https://www.guru99.com/what-is-backend-developer.html" />
         <CardView name="Data Science" field="Software engineering" image={dataScience} btnLink="https://www.oracle.com/what-is-data-science/" />
-        <CardView name="Data Manager" field="Software engineering" image={dataManager} btnLink="https://www.oracle.com/what-is-data-science/" />
-        <CardView name="Project Manager" field="Software management" image={projectManager} btnLink="https://www.oracle.com/what-is-data-science/" />
-        <CardView name="Admin" field="Software administrator" image={administrator} btnLink="https://www.oracle.com/what-is-data-science/" />
-        <CardView name="Game developer" field="Software engineering" image={videoGame} btnLink="https://www.oracle.com/what-is-data-science/" />
+        <CardView name="Data Manager" field="Software engineering" image={dataManager} btnLink="https://www.accenture.com/us-en/careers/jobdetails?id=R00138612_en" />
+        <CardView name="Project Manager" field="Software management" image={projectManager} btnLink="https://www.projectmanager.com/" />
+        <CardView name="Admin" field="Software administrator" image={administrator} btnLink="https://en.wikipedia.org/wiki/System_administrator" />
+        <CardView name="Game developer" field="Software engineering" image={videoGame} btnLink="https://www.simplilearn.com/how-to-become-a-game-developer-article" />
       </div>
     </>
   )
--- src/components/CardView.jsx
+++ src/components/CardView.jsx
@@ -7,7 +7,7 @@ const CardView = (props) =>{
                 <img src={props.image} alt="couldn't load" class="image"/>
                 <h3>{props.name}</h3>
                 <h5>{props.field}</h5>
-                <a href={props.btnLink} target="_blank"><button>Learn More</button></a>
+                <a href={props.btnLink} target="_blank" class="anchor"><button class="button-30">Learn More</button></a>
             </div>
         </div>
     )
